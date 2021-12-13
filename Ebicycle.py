e95466014c1707432fd821208e420c87ed761a50
camryray <95621069+camryray@users.noreply.github.com>
2021-12-13 20:32:00 +0800

    Update Ebicycle.py

--- Ebicycle.py
+++ Ebicycle.py
@@ -12,7 +12,7 @@ class Ebicycle(Bicycle):
         st.write("充電後電量：{}度".format(self.vol))
     def run(self,distance):
          st.write("行駛前電量：{}".format(self.vol))
-        e_distance = self.vol * 10
+         e_distance = self.vol * 10
         if distance <= e_distance:
             self.vol = self.vol - distance/10
             st.write("電動騎行{}km".format(distance))
@@ -21,7 +21,7 @@ class Ebicycle(Bicycle):
             st.write("電動騎行{}km".format(e_distance))
             self.vol = 0
             super().run(distance-e_distance)
-         st.write("行駛後電量：{}".format(self.vol))
+            st.write("行駛後電量：{}".format(self.vol))
  
  
 b = Bicycle()
